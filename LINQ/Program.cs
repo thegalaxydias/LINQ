f5d1d98b2fa140aeed2fae93228ae68673a963d2
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-16 22:00:19 +0300

    3.0

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -11,18 +11,25 @@ namespace LINQ
             List<User> users = new List<User>()
             {
                  new User {role = "admin", login = "carl", password = 123},
-                 new User {role = "customer", login = "tom", password = 890},
-                 new User {role = "manager", login = "jo", password = 000},
+                 new User {role = "customer", login = "asterix", password = 890},
+                 new User {role = "manager", login = "amf", password = 000},
                  new User {role = "user", login = "ron", password = 99},
                  new User {role = "superadmin", login = "jef", password = 00},
             };
 
-            var output1 = users.Where(u => u.role == "admin").FirstOrDefault();
+            var output1 = users.Where(u => u.role == "admin");
             var output2 = users.Where(u => u.login.StartsWith("a"));
             var output3 = users.OrderBy(u => u.login);
-            Console.WriteLine(output1);
-            Console.WriteLine(output2);
-            Console.WriteLine(output3);
+
+            foreach (var u in output1)
+                Console.WriteLine($"output1:\n{u.role} - {u.login} - {u.password}");
+
+            foreach (var u in output2)
+                Console.WriteLine($"--- \noutput2:\n{u.role} - {u.login} - {u.password}");
+
+
+            foreach (var u in output3)
+                Console.WriteLine($"---\noutput3:\n{u.role} - {u.login} - {u.password}");
         }
 
     }
