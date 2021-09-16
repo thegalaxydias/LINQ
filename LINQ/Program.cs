58b0a45a88b2a3a8eb1a96992d992941d504e7b8
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-16 21:39:04 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>()
+            {
+                 new User {role = "admin", login = "carl", password = 123},
+                 new User {role = "customer", login = "tom", password = 890},
+                 new User {role = "manager", login = "jo", password = 000},
+                 new User {role = "user", login = "ron", password = 99},
+                 new User {role = "superadmin", login = "jef", password = 00},
+            };
+
+            var output1 = users.Where(u => u.role == "admin").FirstOrDefault();
+            var output2 = users.Where(u => u.login.StartsWith("a"));
+            Console.WriteLine(output2);
+        }
+
+    }
+
+    public class User {
+
+        public string role { get; set; }
+        public string login { get; set; }
+        public int password { get; set; }
+        
+
+        
+    }
+}
+
+
+        
+      
+
+
+    
